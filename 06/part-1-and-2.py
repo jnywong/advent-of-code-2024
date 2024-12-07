7944706b3c965b2315eb80013c34a59a4255b0ec
jnywong <jnywong.pro@gmail.com>
2024-12-07 13:11:22 +0000

    Update day 6

--- 06/part-1-and-2.py
+++ 06/part-1-and-2.py
@@ -1,24 +1,20 @@
 def get_guard_path(position, direction, n_data):
     i = 0
     d = direction[i]
-    path = [position]
+    path = [[position, d]]
     loop = False
     while 0 <= position[0] < len(data) and 0 <= position[1] < len(data):
         position = [position[0] + d[0], position[1] + d[1]]
         if position in obstruction:
             i += 1
             d = direction[i % len(direction)]
-            position = path[-1]
+            position = path[-1][0]
         else:
-            path.append(position)
-        if is_loop(path) or len(path) > 4 * n_data**2:
+            path.append([position, d])
+        if [position, d] in path[:-2] or len(path) > 4 * n_data**2:
             loop = True
             return path, loop
     return path, loop
-
-
-def is_loop(path):
-    return any(path[-i:] == path[-2 * i : -i] for i in range(2, len(path) // 2 + 1))
 
 
 data = []
@@ -38,26 +34,22 @@ for i in range(len(data)):
         position = [x, y]
 
 # Part 1
-direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+direction = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 i = 0
 d = direction[i]
 path, loop = get_guard_path(position, direction, len(data))
 path.pop()  # remove case that breaks while loop
 
-unique = [list(p) for p in set(tuple(p) for p in path)]
+unique = [list(p) for p in set(tuple(p[0]) for p in path)]
 print(f"Part 1: {len(unique)}")
 
 # Part 2
 
-# path = [[6, 3]]
-loop_pos = []
-for p in path:
+count_loop = 0
+for p in unique:
     obstruction.append(p)
     new_path, loop = get_guard_path(position, direction, len(data))
     if loop:
-        # print(f"{p}")
-        loop_pos.append(p)
-        # print(new_path)
+        count_loop += 1
     obstruction.pop()
-unique_loop_pos = [list(l) for l in set(tuple(l) for l in loop_pos)]
-print(f"Part 2: {len(unique_loop_pos)}")
+print(f"Part 2: {count_loop}")
