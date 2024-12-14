fe9094ace1c1c669b7f88ac1224f17a57b347d17
jnywong <jnywong.pro@gmail.com>
2024-12-14 17:47:30 +0000

    Add part 2

--- 14/part-2.py
+++ 14/part-2.py
@@ -15,8 +15,9 @@ for i in range(len(data)):
     position.append([int(p[0]), int(p[1])])
     velocity.append([int(v[0]), int(v[1])])
 
-nx, ny = 7, 11
-for t in range(100):
+nx, ny = 101, 103
+for t in range(int(1e4)):
+    print(f"t={t}")
     for i in range(len(position)):
         position[i][0] += velocity[i][0]
         position[i][1] += velocity[i][1]
@@ -26,7 +27,6 @@ for t in range(100):
     a = [[" " for _ in range(ny)] for _ in range(nx)]
     for p in position:
         a[p[0]][p[1]] = "#"
-    # input(f"t={t}: Press Enter to continue...")
 
-    with open("output.txt", "w") as file:
-        file.writelines("\n".join("".join(r) for r in a))
+    if any("##########" in "".join(r) for r in a):
+        break
